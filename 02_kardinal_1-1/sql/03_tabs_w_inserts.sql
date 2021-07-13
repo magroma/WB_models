532d3e5b0b5aa85883b36df3d1f54653de655122
Peter <schmidt.multimedia@gmail.com>
2021-07-13 13:06:09 +0200

    update 03 tabs w. inserts 02

--- 02_kardinal_1-1/sql/03_tabs_w_inserts.sql
+++ 02_kardinal_1-1/sql/03_tabs_w_inserts.sql
@@ -5,7 +5,7 @@ DROP DATABASE IF EXISTS mydb;
 CREATE DATABASE mydb;
 USE mydb;
 
--- Mastertabelle: unverändert
+-- Mastertabelle (MT): unverändert
 CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `cat_name` VARCHAR(45) NOT NULL,
@@ -24,3 +24,28 @@ INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (DEFAULT, "Maus
 -- Inhalte: MT
 SELECT * FROM cats;
 
+-- Detailtabelle (DT): Verbindung zur MT über Fremdschlüssel
+CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `servant_name` VARCHAR(45) NOT NULL,
+  `yrs_served` INT NOT NULL,
+  `cats_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_servants_cats_idx` (`cats_id` ASC) VISIBLE,
+  CONSTRAINT `fk_servants_cats`
+    FOREIGN KEY (`cats_id`)
+    REFERENCES `mydb`.`cats` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- Struktur: DT
+DESCRIBE servants;
+
+-- Inserts: DT (Detailtable)
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Peter", 5, 1);
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Juniad", 2, 2);
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Holger", 3, 3);
+
+-- Struktur: DT
+SELECT * FROM servants;
