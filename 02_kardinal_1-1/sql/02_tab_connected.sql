b3b91d889440ac2072eb73ea7b5fa45d0e228d93
Peter <schmidt.multimedia@gmail.com>
2021-07-13 12:07:12 +0200

    update 02_tab

---
+++ 02_kardinal_1-1/sql/02_tab_connected.sql
@@ -0,0 +1,28 @@
+
+-- 2. verbundene Tabellen
+
+-- Mastertabelle: unverändert
+CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cat_name` VARCHAR(45) NOT NULL,
+  `fur_color` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- Detailtabelle: Verbindung zur MT über Fremdschlüssel
+CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `servant_name` VARCHAR(45) NOT NULL,
+  `yrs_served` INT NOT NULL,
+  `cats_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_servants_cats_idx` (`cats_id` ASC) VISIBLE,
+  CONSTRAINT `fk_servants_cats`
+    FOREIGN KEY (`cats_id`)
+    REFERENCES `mydb`.`cats` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+DESCRIBE cats;
+DESCRIBE servants;
