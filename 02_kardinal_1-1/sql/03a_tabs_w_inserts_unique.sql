1e61d77e2bf60a78faede228bbf54a1f8bcf0473
Peter <schmidt.multimedia@gmail.com>
2021-07-14 12:13:36 +0200

    update 1:1

--- 02_kardinal_1-1/sql/03a_tabs_w_inserts_unique.sql
+++ 02_kardinal_1-1/sql/03a_tabs_w_inserts_unique.sql
@@ -5,7 +5,7 @@ DROP DATABASE IF EXISTS mydb;
 CREATE DATABASE mydb;
 USE mydb;
 
--- Mastertabelle: unverändert
+-- Mastertabelle (MT): unverändert
 CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `cat_name` VARCHAR(45) NOT NULL,
@@ -24,14 +24,15 @@ INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (DEFAULT, "Maus
 -- Inhalte: MT
 SELECT * FROM cats;
 
--- Detailtabelle: Verbindung zur MT über Fremdschlüssel
+-- Detailtabelle (DT): Verbindung zur MT über Fremdschlüssel
 CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
-  `srv_name` VARCHAR(45) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `servant_name` VARCHAR(45) NOT NULL,
   `yrs_served` INT NOT NULL,
   `cats_id` INT NOT NULL,
-  PRIMARY KEY (`cats_id`),
-  INDEX `fk_servants_cats_idx` (`cats_id` ASC) VISIBLE,
-  UNIQUE INDEX `cats_id_UNIQUE` (`cats_id` ASC) VISIBLE,
+  PRIMARY KEY (`id`),
+  INDEX `fk_servants_cats_idx` (`cats_id` ASC) ,
+  UNIQUE INDEX `cats_id_UNIQUE` (`cats_id` ASC) ,
   CONSTRAINT `fk_servants_cats`
     FOREIGN KEY (`cats_id`)
     REFERENCES `mydb`.`cats` (`id`)
@@ -43,9 +44,10 @@ ENGINE = InnoDB;
 DESCRIBE servants;
 
 -- Inserts: DT (Detailtable)
-INSERT INTO `mydb`.`servants` (`srv_name`, `yrs_served`, `cats_id`) VALUES ( "Peter", 5, 2);
-INSERT INTO `mydb`.`servants` (`srv_name`, `yrs_served`, `cats_id`) VALUES ( "Josef", 2, 1);
-INSERT INTO `mydb`.`servants` (`srv_name`, `yrs_served`, `cats_id`) VALUES ( "Max", 12, 1);
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Peter", 5, 1);
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Juniad", 2, 2);
+-- Das sollte eigentlich nicht gehen !! --> UNIQUE
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Holger", 3, 1);
 
--- Inhalte: MT
+-- Struktur: DT
 SELECT * FROM servants;
