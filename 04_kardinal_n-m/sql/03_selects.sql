9fbed2f5f16676bdb9698f848c93c73f92e5021d
Peter <schmidt.multimedia@gmail.com>
2021-07-15 10:32:29 +0200

    Create 03_selects.sql

---
+++ 04_kardinal_n-m/sql/03_selects.sql
@@ -0,0 +1,33 @@
+-- 03. SELECTS
+USE mydb;
+
+-- Einzeltabellen
+
+SELECT * FROM cats;
+SELECT * FROM servants;
+SELECT * FROM products;
+SELECT * FROM purchases;
+
+-- Inner Join 1 / Kombi (servants / products / purchases)
+SELECT
+	*
+FROM purchases
+INNER JOIN servants ON servants.id = purchases.servants_id
+INNER JOIN products ON products.id = purchases.products_id
+;
+
+-- Welche Artikel hat Dieter gekauft?
+SELECT
+	#servant_name AS "Diener"
+    #servant_name AS Diener -- Variante 1
+    #servant_name  Diener -- Variante 2
+    #product_name AS "Von Dieter gekaufte Artikel"
+    concat(servant_name, " kauft ", product_name, ".") AS "Kaufhandlung"
+FROM purchases
+INNER JOIN servants ON servants.id = purchases.servants_id
+INNER JOIN products ON products.id = purchases.products_id
+#WHERE servant_name = "Dieter"
+WHERE servant_name = "Peter"
+#WHERE servants_id = 2
+;
+
