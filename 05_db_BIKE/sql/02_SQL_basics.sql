89f09213ab32b8cab829af2254378a343f2a7d9c
Peter <schmidt.multimedia@gmail.com>
2021-07-19 11:22:25 +0200

    05_db_Bike

---
+++ 05_db_BIKE/sql/02_SQL_basics.sql
@@ -0,0 +1,30 @@
+SHOW DATABASES;
+USE bike;
+SHOW TABLES;
+
+DESCRIBE artikel;
+SELECT * FROM artikel;
+
+DESCRIBE auftrag;
+SELECT * FROM auftrag;
+
+DESCRIBE auftragsposten;
+SELECT * FROM auftragsposten;
+
+DESCRIBE kunde;
+SELECT * FROM kunde;
+
+DESCRIBE lager;
+SELECT * FROM lager;
+
+DESCRIBE lieferant;
+SELECT * FROM lieferant;
+
+DESCRIBE lieferung;
+SELECT * FROM lieferung;
+
+DESCRIBE personal;
+SELECT * FROM personal;
+
+DESCRIBE teilestruktur;
+SELECT * FROM teilestruktur;
